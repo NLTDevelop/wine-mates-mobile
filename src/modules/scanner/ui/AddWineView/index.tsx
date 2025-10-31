6ab3b61b17a034742722a03b817714bf2b6a305c
Mykhailo Novikov <mnoviknltdev@gmail.com>
2025-10-31 16:20:45 +0200

    Added partially UI for add wine screen

--- src/modules/scanner/ui/AddWineView/index.tsx
+++ src/modules/scanner/ui/AddWineView/index.tsx
@@ -4,16 +4,23 @@ import { View } from 'react-native';
 import { useUiContext } from '@/UIProvider';
 import { ScreenContainer } from '@/UIKit/ScreenContainer';
 import { Typography } from '@/UIKit/Typography';
+import { HeaderWithBackButton } from '@/UIKit/HeaderWithBackButton';
+import { Button } from '@/UIKit/Button';
 
 export const AddWineView = () => {
-    const { colors } = useUiContext();
+    const { colors, t } = useUiContext();
     const styles = useMemo(() => getStyles(colors), [colors]);
-
     return (
         // <WithErrorHandler error={isAuthError ? ErrorTypeEnum.ERROR : null} onRetry={retrySignIn}>
-        <ScreenContainer edges={['top']}>
+        <ScreenContainer
+            edges={['top']}
+            withGradient
+            headerComponent={<HeaderWithBackButton title={t('scanner.whatsInYourGlass')} />}
+        >
             <View style={styles.container}>
-                <Typography text={'Add Wine Screen'} variant="h3" />
+                <Typography text={t('scanner.enterData')} variant="body_400" style={styles.title} />
+
+                <Button text={t('scanner.tasteWine')} onPress={() => {}} containerStyle={styles.button} />
             </View>
         </ScreenContainer>
         // </WithErrorHandler>
