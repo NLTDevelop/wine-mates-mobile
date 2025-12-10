14d0d405a7c8a0fd3feeba17ad81ea8e430e8e7c
Mykhailo Novikov <mnoviknltdev@gmail.com>
2025-12-10 17:56:53 +0200

    small fixes

--- src/modules/scanner/ui/components/ColorButton/index.tsx
+++ src/modules/scanner/ui/components/ColorButton/index.tsx
@@ -4,7 +4,6 @@ import { getStyles } from './styles';
 import { useUiContext } from '@/UIProvider';
 import { Typography } from '@/UIKit/Typography';
 import { IWineColorShade } from '@/entities/wine/types/IWineColorShade';
-import { getContrastColor } from '@/utils';
 
 interface IProps {
     color: IWineColorShade;
@@ -14,12 +13,11 @@ interface IProps {
 
 export const ColorButton = ({ color, isActive, onPress }: IProps) => {
     const { colors } = useUiContext();
-    const textColor = getContrastColor(color.colorHex);
-    const styles = useMemo(() => getStyles(colors, textColor), [colors, textColor]);
+    const styles = useMemo(() => getStyles(colors, color.colorHex), [colors, color]);
 
     return (
         <TouchableOpacity
-            style={[styles.container, { backgroundColor: color.colorHex }, isActive && styles.activeContainer]}
+            style={[styles.container, isActive && styles.activeContainer]}
             onPress={onPress}
         >
             <Typography variant="body_400" text={color.name} numberOfLines={1} style={styles.text} />
